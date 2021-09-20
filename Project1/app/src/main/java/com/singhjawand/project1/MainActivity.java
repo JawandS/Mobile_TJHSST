faa6ee7bb78e94d681046db8f896cc4887517322
JawandS <jawandsingh@gmail.com>
2021-09-20 12:07:13 -0400

    Lab06 base (From Mr. Tra's GitHub)

--- Project1/app/src/main/java/com/singhjawand/project1/MainActivity.java
+++ Project1/app/src/main/java/com/singhjawand/project1/MainActivity.java
@@ -10,6 +10,11 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
     SharedPreferences prefs;
     SharedPreferences.Editor editor;
@@ -32,6 +37,54 @@ public class MainActivity extends AppCompatActivity {
         phone = findViewById(R.id.phoneText);
         display = findViewById(R.id.display);
         display2 = findViewById(R.id.display2);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    try{
+
+        Map<String, String> map = (Map<String, String>) prefs.getAll();
+        List<String> keysAsArray = new ArrayList<String>(map.keySet());
+        Random r = new Random();
+        String key = keysAsArray.get(r.nextInt(keysAsArray.size()));
+        String val = map.get(key);
+        display.setText(val);
+        name.setText(key);
+
+    }catch (Exception ignored){}
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onResume() {
+        super.onResume();
+        try{
+            display.setText("Email");
+            display2.setText("Phone Number");
+            Map<String, String> map = (Map<String, String>) prefs.getAll();
+            List<String> keysAsArray = new ArrayList<String>(map.keySet());
+            Random r = new Random();
+            String key = keysAsArray.get(r.nextInt(keysAsArray.size()));
+            String val = map.get(key);
+            try{
+                int temp = Integer.parseInt(key);
+                display2.setText(val);
+                for(String k : keysAsArray) {
+                    if (k.hashCode() == temp) {
+                        name.setText(k);
+                        //display.setText(map.get(k));
+                    }
+                }
+
+            }catch (Exception e){
+                display.setText(val);
+                //display2.setText(map.get(String.valueOf(key.hashCode())));
+                name.setText(key);
+            }
+        }catch (Exception ignored){}
+
     }
 
     @SuppressLint("SetTextI18n")
