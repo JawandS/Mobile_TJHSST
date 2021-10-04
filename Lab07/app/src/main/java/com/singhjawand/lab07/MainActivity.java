8a92772583adcd8c86d58c6eeebb7b236760cb65
JawandS <jawandsingh@gmail.com>
2021-10-04 11:28:25 -0400

    Lab07b - fragments

--- Lab07/app/src/main/java/com/singhjawand/lab07/MainActivity.java
+++ Lab07/app/src/main/java/com/singhjawand/lab07/MainActivity.java
@@ -10,19 +10,22 @@ import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
-    FragmentTransaction ft;
+    Fragment[] fragments = {new Fragment()};
+    Bundle instance;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        instance = savedInstanceState;
     }
 
-    public void addFrag(View view) {
+
+    public void add_frag(View view) {
         LinearLayout layout = (LinearLayout)findViewById(R.id.parent);
         FragmentTransaction txn = getSupportFragmentManager().beginTransaction();
-        Fragment[] fragments = {new FooFrag(), new FooFrag(), new FooFrag(), new FooFrag()};
         int i = 1; // This seems really fragile though
         for (Fragment f : fragments) {
             FrameLayout frame = new FrameLayout(this);
@@ -32,11 +35,43 @@ public class MainActivity extends AppCompatActivity {
             i++;
         }
         txn.commit();
+    }
+
+    public void replace_frag(View view) {
+    }
+
+    public void hide_frag(View view) {
+    }
+
+    public void show_frag(View view) {
+    }
+
+    public Fragment[] add_elem(Fragment[] arr, Fragment elem){
+        Fragment[] temp = new Fragment[arr.length + 1];
+        System.arraycopy(arr, 0, arr, 0, temp.length - 1);
+        temp[arr.length] = elem;
+        return temp;
+    }
+}
+
+//    FragmentTransaction ft;
+//public void addFrag(View view) {
+//        LinearLayout layout = (LinearLayout)findViewById(R.id.parent);
+//        FragmentTransaction txn = getSupportFragmentManager().beginTransaction();
+//        Fragment[] fragments = {new FooFrag(), new FooFrag(), new FooFrag(), new FooFrag()};
+//        int i = 1; // This seems really fragile though
+//        for (Fragment f : fragments) {
+//        FrameLayout frame = new FrameLayout(this);
+//        frame.setId(i);
+//        layout.addView(frame);
+//        txn.add(i, f);
+//        i++;
+//        }
+//        txn.commit();
+//        }
 //        ft = getSupportFragmentManager().beginTransaction();
 //        // Replace the contents of the container with the new fragment
 //        ft.add(R.id.framework, new FooFrag());
 //        // or ft.add(R.id.your_placeholder, new FooFragment());
 //        // Complete the changes added above
 //        ft.commit();
-    }
-}
