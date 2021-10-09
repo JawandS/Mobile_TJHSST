cb3af9d360e953568c63ef7b4826de6400bd5ea3
JawandS <jawandsingh@gmail.com>
2021-10-09 19:57:23 -0400

    Lab06, video version

---
+++ Lab06c/app/src/main/java/com/singhjawand/lab06c/MainFragment.java
@@ -0,0 +1,41 @@
+package com.singhjawand.lab06c;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
+
+public class MainFragment extends Fragment {
+    ViewPager2 mViewPager2;
+    int position;
+
+    public static Fragment newInstance(ViewPager2 mViewPager2, int position) {
+        MainFragment fragment = new MainFragment();
+        fragment.mViewPager2 = mViewPager2;
+        fragment.position = position;
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // access the button
+        Button mButton = view.findViewById(R.id.pressme);
+        mButton.setText("Press " + position);
+    }
+
+}
