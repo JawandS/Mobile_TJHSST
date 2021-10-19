470c59752187e28b24364dbf8f2e711ec64aebe7
JawandS <jawandsingh@gmail.com>
2021-10-19 16:46:12 -0400

    row of balls bouncing

--- Lab08/app/src/main/java/com/singhjawand/lab08/DrawView.java
+++ Lab08/app/src/main/java/com/singhjawand/lab08/DrawView.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.ColorSpace;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
@@ -13,11 +14,12 @@ import androidx.annotation.Nullable;
 public class DrawView extends View {
     private final Paint painter = new Paint();
     int right = getRight();
-    int[] pos_x = {100, 300, 500, 600, 750, 900, right - 150};
-    int[] pos_y = {10, 10, 10, 10, 10, 10, 10};
-    int[] radius = {80, 30, 90, 45, 25, 120, 100};
-    int[] dy_vals = {15, 4, 5, 3, 2, 25, 20};
-//    Color[] color_list = {new Color(255, 50, 100)}
+    int[] pos_x = {100, 150, 225, 400, 600, 900};
+    int[] pos_y = {10, 10, 10, 10, 10, 10};
+    int[] radius = {20, 35, 50, 65, 80, 95};
+    int[] dy_vals = {10, 11, 12, 13, 14, 15};
+    int[][] color_values = {{255, 50, 100}, {255, 50, 200}, {255, 0, 100}, {0, 50, 100}, {0, 10, 100}, {50, 255, 50}};
+    int acceleration = 1;
 
     public DrawView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -27,19 +29,27 @@ public class DrawView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        // changes the color of the painter
-        painter.setARGB(250, 255, 50, 100);
 
         for (int counter = 0; counter < 6; counter++) {
+            // changes the color of the painter
+            int[] colors = color_values[counter];
+            painter.setARGB(250, colors[0], colors[1], colors[2]);
             int x = pos_x[counter];
             int y = pos_y[counter];
             int dy = dy_vals[counter];
             int rad = radius[counter];
             canvas.drawCircle(x, y, rad, painter);
             pos_y[counter] = y + dy;
-            if (y < 0 || y > getHeight()) {
-                dy_vals[counter] = dy * -1;
-                pos_y[counter] = y + dy_vals[counter];
+            if (y > getHeight()) {
+                if (dy < 50 && dy > 0)
+                    dy_vals[counter] = ((dy + acceleration) * -1);
+                else if (dy > 0)
+                    dy_vals[counter] = dy * -1;
+                pos_y[counter] = getBottom();
+            } else if (y < 0) {
+                if (dy < 0)
+                    dy_vals[counter] = dy * -1;
+                pos_y[counter] = getTop();
             }
         }
 
@@ -47,6 +57,11 @@ public class DrawView extends View {
         invalidate();
     }
 
+    @Override
+    public void setOnClickListener(@Nullable OnClickListener l) {
+        super.setOnClickListener(l);
+        acceleration += 1;
+    }
 }
 
 //        canvas.drawRect(x-10, y-175, x+10, y-100, new Paint());
