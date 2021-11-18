89e49b4cd5315e37c6e4a0d75a395eab783aed37
jawand_s <jawandsingh@gmail.com>
2021-11-18 12:34:41 -0500

    Broken Scroll View

--- ColorPicker/app/src/main/java/com/singhjawand/colorpicker/MainActivity.java
+++ ColorPicker/app/src/main/java/com/singhjawand/colorpicker/MainActivity.java
@@ -9,16 +9,22 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     EditText red, green, blue;
     TextView r_view, blue_view, g_view;
+    Button color_storage;
+    LinearLayout container;
     ConstraintLayout bg;
-    int r, g, b;
+    ArrayList<ColorObject> colors = new ArrayList<>();
     Random rand;
 
     @Override
@@ -30,9 +36,13 @@ public class MainActivity extends AppCompatActivity {
         green = findViewById(R.id.green);
         blue = findViewById(R.id.blue);
 
+        color_storage = findViewById(R.id.color_storage);
+
         r_view = findViewById(R.id.red_text);
         g_view = findViewById(R.id.green_text);
         blue_view = findViewById(R.id.blue_text);
+
+        container = findViewById(R.id.container);
 
         bg = findViewById(R.id.background);
 
@@ -54,9 +64,13 @@ public class MainActivity extends AppCompatActivity {
 
     @SuppressLint("SetTextI18n")
     public void changeBackground(){
-        r = Integer.parseInt(red.getText().toString());
-        g = Integer.parseInt(green.getText().toString());
-        b = Integer.parseInt(blue.getText().toString());
+
+        int r = Integer.parseInt(red.getText().toString());
+        int g = Integer.parseInt(green.getText().toString());
+        int b = Integer.parseInt(blue.getText().toString());
+
+        colors.add(new ColorObject(r, g, b));
+        update();
 
         r_view.setText(r + "");
         g_view.setText(g + "");
@@ -67,19 +81,57 @@ public class MainActivity extends AppCompatActivity {
 
     @SuppressLint("SetTextI18n")
     public void randomBackground(View view) {
-
+        randomBackground();
     }
 
     @SuppressLint("SetTextI18n")
     public void randomBackground(){
+        ColorObject c = new ColorObject();
+        colors.add(c);
+        update();
+
+        int red = c.r;
+        int green = c.g;
+        int blue = c.b;
+
+        r_view.setText(red + "");
+        g_view.setText(green + "");
+        blue_view.setText(blue + "");
+
+        color_storage.setBackgroundColor(Color.rgb(red, green, blue));
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void update(){
+        ColorObject color = colors.get(colors.size() - 1);
+        TextView temp = new TextView(this);
+        color.setView(temp);
+
+        container.addView(temp);
+    }
+}
+
+class ColorObject{
+    public int r, g, b;
+    Random rand = new Random();
+
+    public ColorObject(){
         r = rand.nextInt(255);
         g = rand.nextInt(255);
         b = rand.nextInt(255);
-
-        r_view.setText(r + "");
-        g_view.setText(g + "");
-        blue_view.setText(b + "");
-
-        bg.setBackgroundColor(Color.rgb(r, g, b));
     }
+
+    public ColorObject(int red, int green, int blue){
+        r = red;
+        g = green;
+        b = blue;
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void setView(TextView temp){
+        temp.setText(r + " " + g + " " + b);
+        temp.setTextSize(50);
+        temp.setBackgroundColor(Color.rgb(r, g, b));
+    }
+
 }
